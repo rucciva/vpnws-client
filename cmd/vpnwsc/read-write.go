134c2e3104a6947b0d57e0f78325218df567d955
Rucciva <rucciva@gmail.com>
2017-12-16 16:04:09 +0700

    read write timer

--- cmd/vpnwsc/read-write.go
+++ cmd/vpnwsc/read-write.go
@@ -2,22 +2,116 @@ package main
 
 import (
 	"context"
+	"errors"
 	"io"
+	"time"
 )
 
-func readWrite(r io.Reader, w io.Writer, buf []byte) (err error) {
-	n, err := r.Read(buf)
-	if err != nil {
-		return err
-	}
-	_, err = w.Write(buf[:n])
-	if err != nil {
-		return err
-	}
-	return nil
+var (
+	errRWTimeOut = errors.New("read or write timeout")
+)
+
+type WriterWithTimeout interface {
+	io.Writer
+	WriteTimeout() <-chan time.Time
+	ResetWriteTimeout()
 }
 
-func readWriteWithContext(ctx context.Context, r io.Reader, w io.Writer, buf []byte) (err error) {
+type ReaderWithTimeout interface {
+	io.Reader
+	ReadTimeout() <-chan time.Time
+	ResetReadTimeout()
+}
+
+type RWTimer struct {
+	rdur, wdur     time.Duration
+	rtimer, wtimer *time.Timer
+}
+
+func NewRWTimer(r, w time.Duration) *RWTimer {
+	return &RWTimer{
+		r, w,
+		time.NewTimer(r), time.NewTimer(w),
+	}
+}
+
+func (rw *RWTimer) WriteTimeout() <-chan time.Time {
+	if rw == nil || rw.wtimer == nil {
+		return nil
+	}
+	return rw.wtimer.C
+}
+
+func (rw *RWTimer) ResetWriteTimeout() {
+	if rw == nil || rw.wtimer == nil {
+		return
+	}
+	if !rw.wtimer.Stop() {
+		<-rw.wtimer.C
+	}
+	rw.wtimer.Reset(rw.wdur)
+}
+
+func (rw *RWTimer) ReadTimeout() <-chan time.Time {
+	if rw == nil || rw.rtimer == nil {
+		return nil
+	}
+	return rw.rtimer.C
+}
+
+func (rw *RWTimer) ResetReadTimeout() {
+	if rw == nil || rw.rtimer == nil {
+		return
+	}
+	if !rw.rtimer.Stop() {
+		<-rw.rtimer.C
+	}
+	rw.rtimer.Reset(rw.rdur)
+}
+
+func (rw *RWTimer) ResetTimeout() {
+	if rw == nil || rw.rtimer == nil || rw.wtimer == nil {
+		return
+	}
+	rw.ResetReadTimeout()
+	rw.ResetWriteTimeout()
+}
+
+func readWrite(r ReaderWithTimeout, w WriterWithTimeout, buf []byte) (err error) {
+	n, rErr, wErr := 0, make(chan error, 1), make(chan error, 1)
+
+	// read with timeout
+	r.ResetReadTimeout()
+	go func() {
+		var err2 error
+		n, err2 = r.Read(buf)
+		rErr <- err2
+	}()
+	select {
+	case err = <-rErr:
+	case <-r.ReadTimeout():
+		err = errRWTimeOut
+	}
+	if err != nil {
+		return
+	}
+
+	// write with timeout
+	w.ResetWriteTimeout()
+	go func() {
+		var err2 error
+		n, err2 = w.Write(buf[:n])
+		wErr <- err2
+	}()
+	select {
+	case err = <-wErr:
+	case <-w.WriteTimeout():
+		err = errRWTimeOut
+	}
+	return
+}
+
+func readWriteWithContext(ctx context.Context, r ReaderWithTimeout, w WriterWithTimeout, buf []byte) (err error) {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
